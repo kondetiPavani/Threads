adf8175420c83bfb940297a01df40f81572cb6ed
kondetiPavani <144420158+kondetiPavani@users.noreply.github.com>
2023-09-08 12:54:23 +0530

    Update deployment_sentiment_analysis.py

--- deployment_sentiment_analysis.py
+++ deployment_sentiment_analysis.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1CK9rORjEow-Qa6B5pzrQAxmcMOYcqzCQ
 """
 
-!pip install -q streamlit
+
 
 import  streamlit as st
 import pickle
